68a6451aa5f5113e679d32836ec0c05d677ab40b
DWest <dawesthoff@gmail.com>
2018-04-21 14:02:30 -0500

    setup authorize
    update to include authorize DW

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
